921977162e65d2928c217b19e1a322ec9c4a0127
Greg Cain <codegregcode@gmail.com>
2023-01-21 18:26:04 +0000

    creates list of galaxies

---
+++ src/lists.js
@@ -0,0 +1,29 @@
+const Galaxy = require('./galaxy');
+const SolarSystem = require('./solarsystem');
+const Lifeform = require('./lifeform');
+
+const galaxyList = [
+    galaxy1 = new Galaxy('Nova Prime'),
+    galaxy2 = new Galaxy('Starlight Expanse'),
+    galaxy3 = new Galaxy('The Phantom Spiral'),
+    galaxy4 = new Galaxy('The Cosmic Rift'),
+    galaxy5 = new Galaxy('The Black Hole Cluster'),
+    galaxy6 = new Galaxy('The Stellar Sea'),
+    galaxy7 = new Galaxy('The Galactica'),
+    galaxy8 = new Galaxy('The Nova Nebula'),
+    galaxy9 = new Galaxy('The Cosmic Storm'),
+    galaxy10 = new Galaxy('The Starry Horizon'),
+    galaxy11 = new Galaxy('The Andromeda Array'),
+    galaxy12 = new Galaxy('The Galaxia'),
+    galaxy13 = new Galaxy('The Ionosphere'),
+    galaxy14 = new Galaxy('The Starfire'),
+    galaxy15 = new Galaxy('The Cosmic Vortex')
+];
+
+
+
+
+
+
+module.exports = galaxyList;
+
